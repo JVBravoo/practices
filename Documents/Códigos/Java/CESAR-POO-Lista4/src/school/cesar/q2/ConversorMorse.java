addb0716e7f0ceb48dd0eda1615139989ecf42be
Joao Victor <jvbm@cesar.school>
2019-06-20 01:06:41 -0300

    Commit 1.0

---
+++ Documents/Códigos/Java/CESAR-POO-Lista4/src/school/cesar/q2/ConversorMorse.java
@@ -0,0 +1,34 @@
+package school.cesar.q2;
+
+import java.util.Scanner;
+
+public class ConversorMorse {
+	
+	public static conversor(){
+		
+		char[] alfabeto = {
+				'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t',
+				'u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','0'
+		};
+
+		String[] morse = {
+				".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-",
+				".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--",
+				"-..-", "-.--", "--..", 
+				/* começo dos números */ ".----", "..---", "...--", "....-",
+				".....", "-....", "--...", "---..", "----.", "-----"
+		};
+		
+		System.out.println("TRADUTOR DE CÓDIGO MORSE");
+		System.out.println("");
+		System.out.println("Digite aqui o que voce deseja ser traduzido de morse para portugues:");
+		System.out.println("");
+		
+		
+		
+		
+	}
+
+}
+
+
